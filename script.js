bcedf39be9af3eb8bafdd28ce71b8c6f972a6092
Aman29web <amansinghvns29@gmail.com>
2024-10-10 12:15:09 +0530

    Done

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ function prevSlide() {
 }
 
 window.addEventListener('resize', updateSliderPosition);
+
+
+// lalala
+
+
+function updateImage() {
+    const img = document.getElementById('responsive-image');
+    if (window.innerWidth <= 600) {
+        img.src = '/assests/wa1.jpg'; // Change to small image
+    } else if (window.innerWidth <= 900) {
+        img.src = '/assests/wa3.jpg'; // Change to medium image
+    } else {
+        img.src = '/assests/w.png'; // Default image
+    }
+}
+
+window.addEventListener('resize', updateImage);
+window.addEventListener('load', updateImage);
